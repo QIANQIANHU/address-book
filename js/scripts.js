a49e2033b09d908d66e73a78decb57e9d459398e
qh <qianhu@pindous.com>
2017-10-31 15:55:43 -0700

    movieticket radio button

--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,39 @@
 //business logic
-function Contact(first, last) {
+function Result(first, last) {
   this.firstName = first;
   this.lastName = last;
-  this.addresses = [];
 }
-
-function Address(street, city, state) {
-  this.street = street;
-  this.city = city;
-  this.state = state;
-}
-
-Contact.prototype.fullName = function() {
+Result.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
-}
-
-Address.prototype.fullAddress = function() {
-  return this.street + ", " + this.city + ", " + this.state;
 }
 
 // user interface logic
 $(document).ready(function() {
-
-  $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address">' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-street">Street</label>' +
-                                   '<input type="text" class="form-control new-street">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-city">City</label>' +
-                                   '<input type="text" class="form-control new-city">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-state">State</label>' +
-                                   '<input type="text" class="form-control new-state">' +
-                                 '</div>' +
-                               '</div>');
-  });
-
-  $("form#new-contact").submit(function(event) {
+  // $("#add-movie").click(function() {
+  //   $(#"new-movies").append("<div class="new-movie">" +
+  //                             "<label></label>" +
+  //                             "<input type="radio" name="moviename">"
+  //                             "</div>" +
+  //                           "</div>");
+  // });
+  $("form#new-info").submit(function(event) {
     event.preventDefault();
+      $(".result").last().click(function() {
+        $("#show-ticket").show();
+        $("#show-ticket h2").text(newResult.firstName);
+        $(".first-name").text(newResult.firstName);
+        $(".last-name").text(newResult.lastName);
+      });
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
+    var moviename = $("input:radio[name=moviename]:checked").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var newResult = new Result(inputtedFirstName, inputtedLastName);
 
-    $(".new-address").each(function() {
-      var inputtedStreet = $(this).find("input.new-street").val();
-      var inputtedCity = $(this).find("input.new-city").val();
-      var inputtedState = $(this).find("input.new-state").val();
-      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
-      newContact.addresses.push(newAddress);
-    });
+    $("ul#ticket").append("<li><span class='result'>" + newResult.fullName() + "</span></li>");
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-
-      $("ul#addresses").text("");
-      newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-      });
-    });
-    function resetFields() {
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
-    }
-
   });
 });
